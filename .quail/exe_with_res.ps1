3ca76a93baed21411ec47608de0324e65dcd060f
dotBlueShoes <matthewstrumillo@gmail.com>
2024-03-20 19:10:47 +0100

    renaming

--- .quail/exe_with_res.ps1
+++ .quail/exe_with_res.ps1
@@ -1,9 +1,11 @@
 param (
-	[string]$project_dir = ""
+	[string]$project_dir = "",
+	[string]$project_name = ""
 )
 
 # Because sources are not connected to the executable
 #  but placed as a linkfile inside build directory...
 #  When running from console we simply need to change the path to build's path.
+
 Set-Location $project_dir
-./oneEngine.exe
+& ./$project_name.exe
