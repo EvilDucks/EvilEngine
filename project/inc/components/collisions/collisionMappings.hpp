84309927fbeff22d9c02793460c2dfced6fd07f7
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2024-06-05 23:36:36 +0200

    Goal reload scene

--- project/inc/components/collisions/collisionMappings.hpp
+++ project/inc/components/collisions/collisionMappings.hpp
@@ -6,15 +6,34 @@
 #define EVILENGINE_COLLISIONMAPPINGS_HPP
 
 #include "global.hpp"
+#include <chrono>
+#include <thread>
+
+using std::operator""s;
 
 namespace COLLISION_MAP {
     void RegisterCallbacks(COLLISION::MANAGER::CM& manager)
     {
-        COLLISION::MANAGER::RegisterCollisionCallback(manager, "testTrigger", COLLISION::MANAGER::CollisionCallback{
+        COLLISION::MANAGER::RegisterCollisionCallback(manager, "Goal", COLLISION::MANAGER::CollisionCallback{
                 .Ref = "GameplayCollision",
                 .Func = [](COLLIDER::Collider collider1, COLLIDER::Collider collider2, glm::vec3 overlap) {
 
                     DEBUG {spdlog::info("test trigger, overlap [x: {0}; y: {1}; z: {2}", overlap.x, overlap.y, overlap.z);}
+
+                    GLOBAL::Destroy();
+                    GLOBAL::Initialize();
+                    if (GLOBAL::inputManager) {
+                        INPUT_MAP::MapInputs(GLOBAL::inputManager);
+                        INPUT_MAP::RegisterCallbacks(GLOBAL::inputManager);
+                    }
+
+                    if (GLOBAL::uiManager) {
+                        UI_MAP::RegisterCallbacks(GLOBAL::uiManager);
+                    }
+
+                    if (GLOBAL::collisionManager) {
+                        COLLISION_MAP::RegisterCallbacks(GLOBAL::collisionManager);
+                    }
 
                     return true;
                 }
