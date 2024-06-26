ce309a8747804f48a64ffa288189f7839e4bc203
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2024-06-27 01:33:15 +0200

    Goal and center fix and first segment

--- project/inc/manager/scenes.hpp
+++ project/inc/manager/scenes.hpp
@@ -81,10 +81,10 @@ namespace MANAGER::SCENES::GENERATOR {
 
 		MAP_GENERATOR::Modifiers modifiers {
 			/*levelLength*/ 				5,
-			/*stationaryTrapsAmount*/ 		1.0f,
+			/*stationaryTrapsAmount*/ 		0.7f,
 			/*windowTrapsAmount*/ 			1.0f,
-			/*checkpointsSpacing*/          0,
-            /*powerUpsSpacing*/             0,
+			/*checkpointsSpacing*/          2,
+            /*powerUpsSpacing*/             3,
 			/*parkourDifficulty*/ 			difficulty,
 			/*windingModuleProbability*/	0.5f
 		};
@@ -93,8 +93,8 @@ namespace MANAGER::SCENES::GENERATOR {
 		mapGenerator->modifiers = modifiers;
 
 		MAP_GENERATOR::LoadModules (mapGenerator, RESOURCES::MANAGER::SEGMENTS);
-		//MAP_GENERATOR::GenerateLevel (mapGenerator);
-        MAP_GENERATOR::GenerateSpecificLevel(mapGenerator, 2);
+		MAP_GENERATOR::GenerateLevel (mapGenerator);
+        //MAP_GENERATOR::GenerateSpecificLevel(mapGenerator, 2);
         
 		segmentsCount = 
 			mapGenerator->_generatedLevelMainBranch.size() + 
